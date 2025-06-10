4c0ec5fd42082fc4e13cccdd8c887a179c441c3e
SaidMLife <said.ibrohim6@gmail.com>
2025-06-10 16:04:25 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
-from selenium import webdriver
 import pytest
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+
 
 @pytest.fixture
 def driver_chrome():
@@ -10,7 +12,7 @@ def driver_chrome():
     options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=options)
     driver.get("https://my.proweb.uz/log-in")
-    
+
     yield driver
 
     driver.quit()
